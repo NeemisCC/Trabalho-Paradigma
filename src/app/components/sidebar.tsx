21fe3a82116ac027ce5327b03b95085e8e995886
Davi Oliveira <davirdoliveira@gmail.com>
2024-09-15 10:24:47 -0300

    created link for pages

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -3,14 +3,27 @@ import * as React from "react"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import MenuSectionTitle from "./menu/section_title"
 import MenuSectionItem from "./menu/section_item"
+import MenuSection from "./menu/menu_section"
 
 export function Sidebar() {
     return (
         <ScrollArea className="h-screen w-60 fixed top-10">
-            <div className="p-4">
-                <MenuSectionTitle text="Seção"/>
-                <MenuSectionItem text="Assunto"/>
-            </div>
+            <MenuSection>
+                <MenuSectionTitle text="P.O.O"/>
+                <MenuSectionItem text="Características"/>
+                <MenuSectionItem text="História"/>
+                <MenuSectionItem text="Pessoas Envolvidas"/>
+                <MenuSectionItem text="Fundamentos"/>
+                <MenuSectionItem text="Objetos e Classes"/>
+                <MenuSectionItem text="Principais Linguagens"/>
+            </MenuSection>
+
+            <MenuSection>
+                <MenuSectionTitle text="Linguagem Java"/>
+                <MenuSectionItem text="História"/>
+                <MenuSectionItem text="Sintaxe"/>
+                <MenuSectionItem text="Semântica"/>
+            </MenuSection>
         </ScrollArea>
     )
 }
